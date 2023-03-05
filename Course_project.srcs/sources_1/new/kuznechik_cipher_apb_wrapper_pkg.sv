5e3bf39f16d7572c647646dabb18727922b98431
IlorDash <ilordash@mail.ru>
2023-03-06 00:45:10 +0300

    src: wrapper_pkg: Add memory map

--- Course_project.srcs/sources_1/new/kuznechik_cipher_apb_wrapper_pkg.sv
+++ Course_project.srcs/sources_1/new/kuznechik_cipher_apb_wrapper_pkg.sv
@@ -1,20 +1,29 @@
 package kuznechik_cipher_apb_wrapper_pkg;
 
-    ///////////////////////
-    // Cipher memory map //
-    ///////////////////////
+  ///////////////////////
+  // Cipher memory map //
+  ///////////////////////
 
-    typedef enum {
-        DATA_OUT = 32'h0014,
-        DATA_IN  = 32'h0004,
-        CONTROL  = 32'h0000
-    } cipher_apb_addr_t;
+  // 0x00 – 0x04 - Control register
+  //    0x00 - RST
+  //    0x01 - {REQ, ACK}
+  //    0x02 - VALID
+  //    0x03 - BUSY
 
-    typedef enum {
-        RST     = 0,
-        REQ_ACK = 1,
-        VALID   = 2,
-        BUSY    = 3
-    } cipher_control_t;
+  // 0x04 - 0x13 - data_in
+  // 0x04 - 0x13 - data_out
+
+  typedef enum {
+    CONTROL  = 32'h0000,
+    DATA_IN  = 32'h0004,
+    DATA_OUT = 32'h0014
+  } cipher_apb_addr_t;
+
+  typedef enum {
+    RST     = 0,
+    REQ_ACK = 1,
+    VALID   = 2,
+    BUSY    = 3
+  } cipher_control_t;
 
 endpackage
